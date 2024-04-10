807470140b54f62a4f5b54d6d0913993d49e6eb1
RapuJeanette <129312358+RapuJeanette@users.noreply.github.com>
2024-04-10 18:08:55 -0400

    Add files via upload

---
+++ Televisor3D/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Televisor3D
+{
+    public class Game: GameWindow
+    {
+        private Tele tele;
+        public Game(int width, int height, string tittle): base(width, height, GraphicsMode.Default, tittle) { }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            GL.Rotate(1.0f, 0.0f, 0.1f, 0.0f);
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(Color4.AliceBlue);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-3.0, 3.0, -3.0, 3.0, -3.0, 3.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+            tele = new Tele(new Vector3(0.3f, -0.2f, -0.05f), 20, 10, 0);
+            tele.dibujar();
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+        protected override void OnResize(EventArgs e) {  
+            GL.Viewport(0,0, Width, Height);
+            base.OnResize(e); 
+        }
+    }
+}
---
+++ Televisor3D/Tele.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Televisor3D
+{
+    internal class Tele
+    {
+        private float alto, largo, ancho;
+        private Vector3 vector; //posicion del objeto
+        public Tele(Vector3 v, float largo, float alto, float ancho)
+        {
+            vector = v;
+            this.alto = alto;
+            this.largo = largo;
+            this.ancho = ancho;
+        }
+
+        public void dibujar()
+        {
+            //PARTE DELANTERA
+            //PANTALLA
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Black);
+            GL.Vertex3(vector.X + 0.03f, vector.Y + 0.04,vector.Z + -0.03);
+            GL.Vertex3(vector.X + 0.03f, vector.Y + 0.36,vector.Z + -0.03);
+            GL.Vertex3(vector.X + 0.47f, vector.Y + 0.36,vector.Z + -0.03);
+            GL.Vertex3(vector.X + 0.47f, vector.Y + 0.04,vector.Z + -0.03);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.DarkBlue);
+            GL.Vertex3(vector.X+ 0f,vector.Y + 0f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0.4f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0f,vector.Y + 0.4f,vector.Z + -0.03f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Yellow);
+            GL.Vertex3(vector.X + 0.22f,vector.Y + 0f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.22f,vector.Y + -0.05f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.26f,vector.Y + -0.05f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.26f,vector.Y + 0f,vector.Z + -0.03f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Yellow);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.05f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.06f,vector.Z +  -0.03f);
+            GL.Vertex3(vector.X + 0.36f,vector.Y + -0.06f,vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.36f,vector.Y + -0.05f, vector.Z + -0.03f);
+            GL.End();
+
+            //PARTE TRASERA DEL TV
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.DarkKhaki);
+            GL.Vertex3(vector.X + 0f,vector.Y + 0f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0.4f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0f, vector.Y + 0.4f, vector.Z + 0f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Aquamarine);
+            GL.Vertex3(vector.X + 0.22f, vector.Y + 0f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.22f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.26f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.26f, vector.Y + 0f, vector.Z + 0f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Aquamarine);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.06f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.36f, vector.Y + -0.06f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.36f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.End();
+
+            //PARTE LATERALES DEL TV
+            //UNIENDO DELANTE CON ATRAS
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.DarkGray);
+            GL.Vertex3(vector.X + 0.0f, vector.Y + 0.0f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.0f, vector.Y + 0.0f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0f, vector.Y + 0.4f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0f, vector.Y + 0.4f, vector.Z + 0f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.DarkGreen);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0.0f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0.0f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0.4f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.5f, vector.Y + 0.4f, vector.Z + 0f);
+            GL.End();
+
+            //UNION DE SOPORTE
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Red);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.05f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.06f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.12f, vector.Y + -0.06f, vector.Z + 0f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Red);
+            GL.Vertex3(vector.X + 0.36f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.36f, vector.Y + -0.05f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.36f, vector.Y + -0.06f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.36f, vector.Y + -0.06f, vector.Z + 0f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Red);
+            GL.Vertex3(vector.X + 0.22f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.22f, vector.Y + -0.05f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.22f, vector.Y + 0f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.22f, vector.Y + 0f, vector.Z + 0f);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color4(Color.Red);
+            GL.Vertex3(vector.X + 0.26f, vector.Y + -0.05f, vector.Z + 0f);
+            GL.Vertex3(vector.X + 0.26f, vector.Y + -0.05f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.26f, vector.Y + 0f, vector.Z + -0.03f);
+            GL.Vertex3(vector.X + 0.26f, vector.Y + 0f, vector.Z + 0f);
+            GL.End();
+
+        }
+    }
+}
